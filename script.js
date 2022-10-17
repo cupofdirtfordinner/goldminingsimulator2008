3b3a360969e85f9c50217746c01d4ef994826865
cupofdirtfordinner <75740323+cupofdirtfordinner@users.noreply.github.com>
2022-10-17 18:24:35 +0000

    stuff... things,..

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 let gold = 0;
 let miners = 0;
-let gps = 0;
+let gps = miners / 20;
+let wagecost = miners * 12;
 
-var intervalId = window.setInterval(function(){
-  makegoldMiners();
-}, 1000);
+const mines = document.getElementById('mines');
 
-var intervalId2 = window.setInterval(function(){
-  updateGoldCount();
-}, 20);
+//var minerElement = document.createElement(img);
+//minerElement.setAttribute('src', 'images/miner.png'); //L
 
-function updateGoldCount() {
-  document.getElementById("goldcounter").innerHTML = gold;
+function createImageMiner() {
+  var createminerimage = document.createElement('img');
+  createminerimage.src =
+    'images/miner.png';
+  document.getElementById('mines').appendChild(img);
 }
 
+var goldperseconds = window.setInterval(function() {
+  makegoldMiners();
+}, 20000);
+
+function updateStats() {
+  document.getElementById("goldcounter").innerHTML = gold;
+  document.getElementById("minercount").innerHTML = miners;
+  document.getElementById("gpscount").innerHTML = Math.round(gps * 100) / 100;
+}
+
+var updateTimer = window.setInterval(function() {
+  updateStats();
+}, 20);
+
+
+
 function goldbutton() {
-  gold += 1;
+  gold += 100;
 }
 
 function addminers() {
@@ -23,9 +40,10 @@ function addminers() {
   if (gold >= 100) {
     miners += 1
     gold -= 100;
-    gps += 1;
+    gps += (0.05);
+    createImageMiner();
   } else {
-
+    console.log("FUCK!")
   }
 }
 function makegoldMiners() {
@@ -33,8 +51,5 @@ function makegoldMiners() {
     for (let i = 0; i < miners; i++) {
       gold += 1;
     }
-  } else {
-    // do nothing
   }
 }
-
