f0e765735775d1da14fe99b020555619baa4064a
cupofdirtfordinner <75740323+cupofdirtfordinner@users.noreply.github.com>
2022-12-05 19:05:13 +0000

    we basically added nothing i think

--- script.js
+++ script.js
@@ -2,54 +2,76 @@ let gold = 0;
 let miners = 0;
 let gps = miners / 20;
 let wagecost = miners * 12;
+let minercooldown = 20000 / miners;
+let minercost = 100;
 
 const mines = document.getElementById('mines');
+const minerimg = document.getElementById('minerimgid');
 
-//var minerElement = document.createElement(img);
-//minerElement.setAttribute('src', 'images/miner.png'); //L
+var img = document.createElement("img");
+img.src = "images/miner.png";
 
-function createImageMiner() {
-  var createminerimage = document.createElement('img');
-  createminerimage.src =
-    'images/miner.png';
-  document.getElementById('mines').appendChild(img);
+var minerproduction = window.setInterval(function() { minersmakegold(); }, minercooldown);
+
+/*var wage = window.setInterval(function() {
+  wage();
+}, 300000);
+
+function wage() {
+  if (gold >= miners * 10) {
+    gold -= miners * 10;
+  } else {
+    //add code here for removing certain amount of miners
+  }
+}*/
+
+// ^ commented bc we dont know how to do this yet -L
+
+function upgrade1() {
+  if (gold >= 300) {
+    gold -= 300;
+    minercooldown -= 10000;
+    wagecost = wagecost * 1.7;
+    gps = gps * 2;
+
+    //this upgrade works now
+  }
 }
 
-var goldperseconds = window.setInterval(function() {
-  makegoldMiners();
-}, 20000);
-
-function updateStats() {
-  document.getElementById("goldcounter").innerHTML = gold;
-  document.getElementById("minercount").innerHTML = miners;
-  document.getElementById("gpscount").innerHTML = Math.round(gps * 100) / 100;
-}
-
-var updateTimer = window.setInterval(function() {
-  updateStats();
-}, 20);
-
-
+var updateTimer = window.setInterval(function() { updateStats(); }, 20);
 
 function goldbutton() {
   gold += 100;
 }
 
 function addminers() {
-  console.log("Adding miner..");
   if (gold >= 100) {
     miners += 1
-    gold -= 100;
+    gold -= minercost;
     gps += (0.05);
-    createImageMiner();
-  } else {
-    console.log("FUCK!")
   }
 }
-function makegoldMiners() {
+function minersmakegold() {
   if (miners >= 1) {
-    for (let i = 0; i < miners; i++) {
-      gold += 1;
-    }
+    gold += 1;
   }
+}
+
+var goldchecker = window.setInterval(function() { checkgold(); }, 1000);
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+function checkgold() {
+  let goldlast = gold;
+  delay(1000);
+  let goldnew = gold;
+  let newgps = goldlast - goldnew;
+} //the goal was this would measure the change in gold in the last second and set gps to that, but it just don't work so idk -L
+
+function updateStats() {
+  document.getElementById("goldcounter").innerHTML = gold;
+  document.getElementById("minercount").innerHTML = miners;
+  document.getElementById("gpscount").innerHTML = Math.round(newgps * 100) / 100;
 }
